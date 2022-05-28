45750f49c719288111d527d2ef54df92fcb93a61
DevotedLynx <68854051+DevotedLynx@users.noreply.github.com>
2022-05-28 23:30:33 +1000

    Main finished

---
+++ addons/main/config.cpp
@@ -0,0 +1,38 @@
+#include "\x\astrid\addons\main\script_component.hpp"
+
+class CfgPatches
+{
+    class astrid_main
+    {
+        name = "Astrid's Boxloader Ace Compat";
+        author = "LynxAustralis";
+        url = "https://github.com/DevotedLynx/AstridsAdvancedAids/wiki";
+        units[] = 
+        {
+        };
+        Weapons[] = 
+        {
+        };
+        requiredVersion = 1.00;
+        version = 0.1;
+        requiredAddons[]=
+        {
+            "ace_main",
+            "boxloader",
+            "cba_main"
+        };
+    };
+};
+class CfgMods
+{
+    class astrid
+    {
+        dir="@AstridsBoxloaderCompat";
+		name="Astrid's Boxloader Ace Compat";
+		hidePicture="true";
+		hideName="true";
+		actionName="Website";
+		action="https://github.com/DevotedLynx/Astrid-Boxloader-Ace-Compat/wiki";
+		description="";
+    };
+};
---
+++ addons/main/script_component.hpp
@@ -0,0 +1,3 @@
+#define COMPONENT main
+#include "\x\ABAC\addons\main\script_mod.hpp"
+#include "\x\ABAC\addons\main\script_macros.hpp"
